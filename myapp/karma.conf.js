0e63705b7d00c3df4f59fe8ae54236c36358841b
Smith <dominique.smith@sap.com>
2021-12-09 19:47:06 -0500

    reconfiguration

--- myapp/karma.conf.js
+++ myapp/karma.conf.js
@@ -1,11 +1,11 @@
 // karma.conf.js
 module.exports = function(config) {
     config.set({
-      basePath: '**/*.js',
-      frameworks: ['jasmine'],
-      singleRun: true,
-      reporters: ['dots', 'mocha'],
-      junitReporter: {outputFile: 'test-results.xml'},
-      //...
+      frameworks: ['mocha'],
+  
+      files: [
+        '*.js'
+      ]
     });
   };
+  
